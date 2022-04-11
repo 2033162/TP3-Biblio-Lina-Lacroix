4f14fc8704b22012a747aa27ac92b98d46f9075e
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-04-11 13:07:53 -0400

    création d'une nouvelle classe amende

---
+++ src/main/java/com/lina/programme_tp3_biblio/modele/Amende.java
@@ -0,0 +1,30 @@
+package com.lina.programme_tp3_biblio.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Amende {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client")
+    @ToString.Exclude
+    private Client client;
+
+    private long unJourAmende = 2L;
+    private long sommeAmende;
+
+    public Amende(Client client, long unJourAmende, long sommeAmende) {
+        this.client = client;
+        this.unJourAmende = unJourAmende;
+        this.sommeAmende = sommeAmende;
+    }
+}
